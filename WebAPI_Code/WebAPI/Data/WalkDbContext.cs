f92147b54198eb31fa184565d9b4f09f462895ef
hphuonggg <karmakute16@gmail.com>
2023-01-31 10:19:18 +0700

    End Of Section 2

---
+++ WebAPI_Code/WebAPI/Data/WalkDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Model.Domain;
+
+namespace WebAPI.Data
+{
+    public class WalkDbContext :DbContext
+    {
+        public WalkDbContext(DbContextOptions<WalkDbContext>options):base(options)
+
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
---
+++ WebAPI_Code/WebAPI/Model/Domain/Walk.cs
@@ -0,0 +1,15 @@
+﻿namespace WebAPI.Model.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }  
+        public Guid RegionIdP { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        //navigation
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
